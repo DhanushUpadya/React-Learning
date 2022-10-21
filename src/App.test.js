933d0e24ddb766d30aa6e5f35661072024cfa4f5
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-10-21 15:41:54 +0530

    Added another test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,16 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link');
+  expect
+  (linkElement).toBeInTheDocument();
 });
+
+test('url should be correct ', () => {
+  render(<App/>);
+  const linkElement = screen.getByTestId('learn-link');
+ // const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.href).toContain('triconinfotech.com')
+})
+
